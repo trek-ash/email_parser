ba40eefd24e5bf483e685378100a572cc4814fd3
trek-ash <yashsinghgreat@gmail.com>
2020-07-17 18:12:12 +0530

    Readme updated, updated comments

--- readme.md
+++ readme.md
@@ -16,7 +16,7 @@ To make the solution scalable, I could think of following: <br />
 1. Since some of the organisations have their email template, and they replace data in their templates to send emails to clients, so handling the templates might help us to tackle a greater range of emails from an organisation.
 2. To make it work for across different organisations templates, we might require inputs from the organisations or our representatives to point out the data and kind of data to extract from the email.
 3. So this leads to providing an interface(like zapier) where the person can select the data and type of data from the given email, and we can use this to make references for further emails in that format.
-4. When we represent the email in such interface, we can have our own elements or identifiers on the elements that will help us track the information for further templates. For e.g. While rendering email to the interface, we can add custom attributes to elements in the email. Once a person selects a word "Yash" as "Name" at a particular element. We can have an element around this text and give it attribute that reflects it is "Name". Also, our custom attributes on the parent element of this text will map the data which we have inside this parent element.( I can think of way to track the particular text as key based on the position of the word. Though we can try to improve on this if possible)
+4. When we represent the email in such interface, we can have our own elements or identifiers ( or comments) on the elements that will help us track the information for further templates. For e.g. While rendering email to the interface, we can add custom attributes to elements in the email. Once a person selects a word "Yash" as "Name" at a particular element. We can have an element around this text and give it attribute that reflects it is "Name". Also, our custom attributes on the parent element of this text will map the data which we have inside this parent element.( I can think of way to track the particular text as key based on the position of the word. Though we can try to improve on this if possible)
 5. For further emails of the same template, we can map the kept information from the previous email and 
 pre-highlight the texts to show to the user. If it's incorrrect, user can reselect the correct text for the data.
 
@@ -37,12 +37,17 @@ For part 2, making solution close to the approach shared above in scalable solut
 ### Progress in part 3:
 
 #### Approach a
-1.  Try to parse the html such that elements can be accessed through selectors (Trying cheerio for the same, extracting the table element).
-2.  Convert the table element to json to parse the strings.
-2.  Access these elements and match to get the properties.
+1. Convert the given HTML content to an array, with each line as an element in the array
+2. Access each line to match for the comments
+3. If any of the specified comments are matched, access the line which has the required detail
+4. Extract the required information from this line.
 
-Issues: 
+##### Issue with this approach: 
+1. Dependence on comments
+2. Specifing the line statically
 
-accessing the exact elements
 
-
+## Run the solution
+Please follow given instructions to run the given solution: 
+1. npm install
+2. npm start
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
-const HtmlTableToJson = require('html-table-to-json');
 
-let name_pattern = /^Name*/g
-let phone_email_ref_pattern = /Every minute counts, make contact as soon as possible/g;
 let result_through_comments = []
 let name_comment_pattern = /<!-- name -->/
 let description_comment_pattern = /<!-- Showing request Description -->/
@@ -13,78 +10,12 @@ fs.readFile('./sample_email.html',"utf-8", (err, data)=>{
     if(err)
         throw err;
     const email_content = data;
-    // Not so good approach
-    // handleEmailContentByConvertingToJSON(email_content);
 
-    // A much better approach
     handleEmailContentUsingText(email_content);
 })
 
-const handleEmailContentByConvertingToJSON = (content) => {
-    // converting the given HTML to DOM, to access elements efficiently
-    const $ = cheerio.load(content);
-    // selecting table elements 
-    const table_data = $("table table table").toString()
-    // removing empty lines
-    table_data.replace("\n", " ")
-    // Parising the table elements to json, to access contents inside the table
-    const jsontable = HtmlTableToJson.parse(table_data)
-    // filtering the empty rows
-    let trimmed_results = jsontable.results.filter(result=>{        
-        return result.length!=0
-    })
-    // 
-    let res = getDetailsFromConvertedJSON(trimmed_results)
-    console.log(res)
-}   
-
-const getDetailsFromConvertedJSON = (trimmed_results) => {
-
-   
-    let res = [];
-
-    trimmed_results = trimmed_results.map((data, p_i)=>{
-        data.forEach((text, i) => {
-            // accessing table rows converted to Json
-            for(let key of Object.keys(text))
-            {   
-                if(text[key].length>0){
-                    // Regex Matching for name
-                    let name_match = text[key].match(name_pattern)
-                    // Regex Matching for contact ref , this will let us extract: address, beds and baths  
-                    let email_and_phone_match = text[key].match(phone_email_ref_pattern)
-
-                    // if name is found, push the trimmed name
-                    if(name_match!=null)
-                        res.push({name: text[key].substring(5)})
-                    
-                    // if email and phone ref is found, push the phone and email from next rows
-                    if(email_and_phone_match!=null){
-                        if(data[i+2][key])
-                            res.push({email: data[i+2][key]})
-                        if(data[i+1][key])
-                            res.push({phone: data[i+1][key]})
-
-                        // Accessing beds and baths from the contact ref
-                        if(trimmed_results[p_i+1]){
-                            // spliting on line break to get address
-                            let address_bed_baths = trimmed_results[p_i+1][0][1].split("\n")
-                            const address = address_bed_baths[0]
-                            let beds_baths = address_bed_baths[2].trim()
-                            const beds = beds_baths.split(" ")[1]
-                            const baths = beds_baths.split(" ")[3]
-                            res.push({beds}, {baths}, {address})
-                        }
-                    }
-
-                }
-            }
-        });
-    })
-
-    return res;
-}
-
+// Function to convert the given html content to array,
+//  splitting it by line and then accessing each line for the required patterns
 const handleEmailContentUsingText = (content) => {
     // Convert the document to array with each line as an entry to array; remove empty lines; remove trailing spaces 
     const line_array = content.split("\n").filter(line_text=>line_text.length>0).map(text=>text.trim())
@@ -95,7 +26,8 @@ const handleEmailContentUsingText = (content) => {
     // results through comments now has the required details
     console.log(result_through_comments)
 }
-// Method to check for comments in a given line, and then access the correct line for the required details 
+
+// Function to check for comments in a given line, and then access the correct line for the required details 
 // @content: content of particular line
 // @lineNo: current line number in the array
 // @arrayRef: reference of array containing the whole document (to access lines for details based on the current comment occurence)
@@ -103,25 +35,35 @@ const extractRequiredInfo = (content, lineNo, arrayRef) => {
     let name_comment = content.match(name_comment_pattern);
     let description_comment = content.match(description_comment_pattern)
     let listing_details_comment = content.match(listing_details_pattern);
-
+    // On name comment matched
     if(name_comment){
         let name_line = arrayRef[lineNo+3]
-        let name = name_line.substring(name_line.indexOf("<strong>")+ 8, name_line.indexOf("</strong>"))
+
+        // Using cheerio to access the HTML sub elements efficiently
+        let $name = cheerio.load(name_line)
+
+        let name = $name("strong").text()
         result_through_comments.push({name})
     }
+    // On description comment matched
     if(description_comment) {
         let phone_line = arrayRef[lineNo+10]
         let email_line = arrayRef[lineNo+17]
+
+        // Using cheerio to access the HTML sub elements efficiently
         let $phone = cheerio.load(phone_line)
         let $email = cheerio.load(email_line)
+        
         let phone = $phone("a").text()
         let email = $email("a").text()
         result_through_comments.push({email}, {phone})
     }
+    // On listing details comment match
     if(listing_details_comment) {
         let address_line = arrayRef[lineNo+19]
         let beds_baths_line = arrayRef[lineNo+21]
 
+        // Using cheerio to access the HTML sub elements efficiently
         let $address = cheerio.load(address_line)
         let $beds_baths = cheerio.load(beds_baths_line)
 
